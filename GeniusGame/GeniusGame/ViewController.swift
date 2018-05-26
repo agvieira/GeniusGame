00f630f56e2ea3bfacda18ee9207f453ad78b82e
Andre Vieira <vieira.agp@gmail.com>
2018-05-26 08:15:47 -0300

    create project

---
+++ GeniusGame/GeniusGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeniusGame
+//
+//  Created by Andre Vieira on 26/05/18.
+//  Copyright © 2018 Andre Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
